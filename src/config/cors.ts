3fcec02fb1ac1414ce159ee3c1080dce390d192f
nelson nopssa <hnopssita@gmail.com>
2026-03-19 15:08:24 -0500

    commit cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,21 @@
-import { CorsOptions } from "cors"
+import { CorsOptions } from "cors";
 
+// Definimos la lista de dominios permitidos
 const whitelist = [
-  "http://localhost:5173",
-  "http://localhost:3000"
-]
+    "http://localhost:5173", 
+    "http://localhost:3000", 
+    process.env.FRONTEND_URL 
+];
 
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
-
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error("Error de CORS"))
+    origin: function (origin, callback) {
+        // 1. Permitir peticiones sin origen (como Postman o herramientas de servidor)
+        // 2. O si el origen está en nuestra lista blanca
+        if (!origin || whitelist.includes(origin)) {
+            callback(null, true);
+        } else {
+            // Si el dominio no está en la lista, lanzamos el error de seguridad
+            callback(new Error("Error de CORS: Origen no permitido por ADN DATA"));
+        }
     }
-
-  }
-}
+};
