68962dc82f236e8dd48170c03f60b563b4c580f5
nelson nopssa <hnopssita@gmail.com>
2026-03-19 20:55:48 -0500

    commit pass3

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,15 +1,19 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config()
+
 const config = () => {
     return {
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT),
-        secure: false, // CAMBIAR A FALSE PARA EL PUERTO 587
+        secure: true, 
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
-        },
-        tls: {
-            rejectUnauthorized: false // Esto evita errores de certificados en Render
         }
     }
 }
+
+// CRÍTICO: Debe tener la palabra "export" al inicio
+export const transporter = nodemailer.createTransport(config());
 
