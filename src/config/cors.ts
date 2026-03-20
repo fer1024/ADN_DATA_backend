c547c667d0034ff7df911ecee5f26308107b5e28
nelson nopssa <hnopssita@gmail.com>
2026-03-19 19:41:59 -0500

    commit smtp

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,7 +3,7 @@ import { CorsOptions } from 'cors' // <-- Esta es la línea que te falta
 const whitelist = [
   "http://localhost:5173",
   process.env.FRONTEND_URL, // Intenta leer de Render
-  "https://tayka.shop"      // Respaldo directo (Hardcoded)
+  "https://tayka.shop"      
 ].filter(Boolean);
 
 export const corsConfig: CorsOptions = {
