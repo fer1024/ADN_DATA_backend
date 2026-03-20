27b0ca320a5aa6519d97ebdc207d119126597d6e
nelson nopssa <hnopssita@gmail.com>
2026-03-19 21:16:33 -0500

    commit pass4

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -1,19 +1,15 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
-dotenv.config()
-
+// En src/config/nodemailer.ts
 const config = () => {
     return {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: true, 
+        host: process.env.SMTP_HOST, // smtp.gmail.com
+        port: 587, // Cambia 465 por 587
+        secure: false, // OBLIGATORIO: cambiar a false si usas 587
         auth: {
             user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
+            pass: process.env.SMTP_PASS // Las 16 letras de Google
+        },
+        tls: {
+            rejectUnauthorized: false // Esto permite que la conexión pase por el firewall de Render
         }
     }
 }
-
-// CRÍTICO: Debe tener la palabra "export" al inicio
-export const transporter = nodemailer.createTransport(config());
-
